a10327905dac1e9ab00af58a0a068eb933184b18
Cheesey <77486662+Cheesey-dev@users.noreply.github.com>
2022-01-26 13:48:03 +0300

    Update shard.js

--- shard.js
+++ shard.js
@@ -8,7 +8,7 @@ const webhook_id = ''
 const webhook_token = ''
 manager.on('shardCreate', async(i) => {
 console.log(`${i.id} / 2 Id' li Shard kullanıma hazır!`)
-const shardlog = new Discord.WebhookClient(webhook_id, webhook_token);
+const shardlog = new Discord.WebhookClient({id: webhook_id, token: webhook_token});
 const embed = new Discord.MessageEmbed() 
 .setDescription(`<:sgs_tick:921392926683197460> **${i.id} / 2** Id' li Shard kullanıma hazır!`)
 .setFooter(`Space Giveaway`, client.user.avatarURL())
@@ -16,6 +16,7 @@ const embed = new Discord.MessageEmbed()
 shardlog.send({embeds: [embed]});
 })
 manager.on('shardDisconnect', async(i) => {
+const shardlog = new Discord.WebhookClient({id: webhook_id, token: webhook_token});
 const embed = new Discord.MessageEmbed() 
 .setDescription(`<:sgs_error:921392927568195645> **${i.id} / 2** Id' li Shard' ın bağlantısı kesildi!`)
 .setFooter(`Space Giveaway`, client.user.avatarURL())
@@ -23,6 +24,7 @@ const embed = new Discord.MessageEmbed()
 shardlog.send({embeds: [embed]});
 })
 manager.on('shardReconnecting', async(i) => {
+const shardlog = new Discord.WebhookClient({id: webhook_id, token: webhook_token});
 const embed = new Discord.MessageEmbed() 
 .setDescription(`<:sgs_tick:921392926683197460> **${i.id} / 2** Id' li Shard yeniden bağlandı!`)
 .setFooter(`Space Giveaway`, client.user.avatarURL())
