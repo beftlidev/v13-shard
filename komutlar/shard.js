90ad3e14f8a7ee6f0e443f5bbe493d3f3a0cadb0
Cheesey <77486662+Cheesey-dev@users.noreply.github.com>
2022-01-26 13:46:37 +0300

    Update shard.js

--- komutlar/shard.js
+++ komutlar/shard.js
@@ -2,9 +2,9 @@
 //kendi komut template nize ekleyiniz.
 
 const promises = [
-	client.shard.fetchClientValues('guilds.cache.size'),
-	client.shard.broadcastEval('this.guilds.cache.reduce((acc, guild) => acc + guild.memberCount, 0)'),
-];
+client.shard.fetchClientValues('guilds.cache.size'),
+client.shard.broadcastEval(c => c.guilds.cache.reduce((acc, guild) => acc + guild.memberCount, 0)),
+		];
   Promise.all(promises)
 	.then(results => {
 		const guildd = results[0].reduce((acc, guildCount) => acc + guildCount, 0);
